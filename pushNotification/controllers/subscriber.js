030b9932570b2741cb249ef768c4f02fe405d974
Helegbedey <devaze007@gmail.com>
2020-05-24 03:30:35 +0100

    completed the collection subscriber service

--- pushNotification/controllers/subscriber.js
+++ pushNotification/controllers/subscriber.js
@@ -45,4 +45,51 @@ router.post('/subscribe', async (req, res) => {
     }
 });
 
+router.post('/notify-collection', async (req, res) => {
+    const {
+        recipientIds,
+        senderId,
+        message
+    } = req.body;
+    try {
+        // Check notification type
+        const getType = await NotificationType.findOne({name: 'subscribe'});
+        if(getType){
+            recipientIds.forEach(recipient => {
+                const newNotification = new Notification({
+                    type: getType.name,
+                    recipientId: recipient,
+                    senderId,
+                    message
+                });
+                newNotification.save();
+                return res.status(200).send('notification sent');                
+            });
+        }else{
+            const newNotificationType = new NotificationType({
+                name: 'collection',
+                description: 'notification for anything related to collection'
+            });
+            newNotificationType.save()
+            .then((response) => {
+                recipientIds.forEach(recipient => {
+                    const newNotification = new Notification({
+                        type: response.name,
+                        recipientId: recipient,
+                        senderId,
+                        message
+                    });
+                    newNotification.save();
+                    return res.status(200).send('notification sent');                
+                });
+            }).catch((error)=> {
+                throw error;
+            })
+        }
+    } catch (error) {
+        throw error 
+    }
+});
+
+
 module.exports = router;
