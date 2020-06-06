aab34671d6d63e886a417a1c4f923a8d54ab201a
Helegbedey <devaze007@gmail.com>
2020-06-06 15:16:27 +0100

    setting up email microservice

---
+++ emails/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/notification/sms/verify', phoneVerify);
+const PORT = 5002;
+app.listen(PORT, () => {
+    console.log(`Sms system now listening on port ${PORT}`)
+});
