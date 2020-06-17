c83330fda61cff8fcc915765ea5f02c48e06b1e6
Helegbedey <devaze007@gmail.com>
2020-06-17 10:21:59 +0100

    added the message endpoint

--- pushNotification/index.js
+++ pushNotification/index.js
@@ -9,6 +9,7 @@ const review = require('./controllers/review');
 const post = require('./controllers/post');
 const comment = require('./controllers/comment');
 const reply = require('./controllers/reply');
+const message = require('./controllers/message');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,6 +29,7 @@ app.use('/notification/push/reviews', review);
 app.use('/notification/push/posts', post);
 app.use('/notification/push/comments', comment);
 app.use('/notification/push/replies', reply);
+app.use('/notification/push/message', message);
 const PORT = 5003;
 app.listen(PORT, () => {
     console.log(`notification system now listening on port ${PORT}`)
